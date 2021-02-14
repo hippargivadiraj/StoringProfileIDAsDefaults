67c416407bf6b3540a205f40a91c2777e356ebc1
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-02-14 11:18:42 -0800

    Initial Commit

---
+++ StoringProfileIDAsDefaults/StoringProfileIDAsDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoringProfileIDAsDefaultsApp.swift
+//  StoringProfileIDAsDefaults
+//
+//  Created by Vadiraj Hippargi on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct StoringProfileIDAsDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
